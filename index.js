3eb9f3535fd31d05ad6555eb43a7849ee7601716
NyakundiMauya <n.mauya.nm@gmail.com>
2023-03-19 12:27:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity1 = "go to the office") {
+   return `This Monday, I will ${activity1}.`;
+}
+function wrapAdjective(param) {
+   if (param === '*') {
+      return function (late = "very late") {
+         return `You are *${late}*!`;
+      }
+   } else if (param === `||`) {
+      return function (late = "very late") {
+         return `You are ||${late}||!`;
+      }
+
+   }
+   //return
+
+}
+
+
